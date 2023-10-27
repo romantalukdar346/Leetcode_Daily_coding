59f1e48a2f76afc6129529bca9824846e415bf24
Roman Talukdar <93270252+romantalukdar346@users.noreply.github.com>
2023-10-28 00:37:28 +0530

    Add files via upload

---
+++ DAY_18/392_Is_Subsequence.py
@@ -0,0 +1,12 @@
+def isSubsequence(s, t):
+    i=j=0
+    while i <len(s) and j <len(t):
+        if s[i]!=t[j]:
+            j+=1
+        else:
+            i+=1
+            j+=1
+    if i==len(s):
+        return True
+    else:
+        return False
