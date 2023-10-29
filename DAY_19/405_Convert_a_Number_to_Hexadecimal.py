445089ad4051913042bfbb5e1aebbafdc7bd5a4e
Roman Talukdar <93270252+romantalukdar346@users.noreply.github.com>
2023-10-29 07:54:50 +0530

    Add files via upload

---
+++ DAY_19/405_Convert_a_Number_to_Hexadecimal.py
@@ -0,0 +1,12 @@
+def toHex(num):
+    if num == 0: return '0'
+    map = '0123456789abcdef'
+    result = ''
+    if num<0: num += 2 ** 32
+    while num > 0:
+        digit = num % 16
+        num = (num-digit) // 16
+        result += str(map[digit])
+    return result[::-1]
+n=int(input())
+print(toHex(n))
