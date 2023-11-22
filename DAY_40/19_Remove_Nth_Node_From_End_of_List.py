fab0da0e1fca49d6e017d0a5460a5103bbf4e63f
Roman Talukdar <93270252+romantalukdar346@users.noreply.github.com>
2023-11-22 22:51:01 +0530

    Add files via upload

---
+++ DAY_40/19_Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,13 @@
+def removeNthFromEnd(self, head, n):
+    if not head:
+        return []
+    fast,slow=head,head
+    for i in range(n):
+        fast=fast.next
+    if not fast:
+        return head.next
+    while fast.next:
+        slow=slow.next
+        fast=fast.next
+    slow.next=slow.next.next
+    return head
