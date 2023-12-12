14ae81a310172ac57fbbfc9158aa9e53e1030620
Roman Talukdar <93270252+romantalukdar346@users.noreply.github.com>
2023-12-12 19:51:17 +0530

    Add files via upload

---
+++ DAY_53/1688_Count_of_Matches_in_Tournament.py
@@ -0,0 +1,11 @@
+def numberOfMatches(self, n):
+
+    match=0
+    while n>1:
+        if n%2==0:
+            match+=n/2
+            n=n/2
+        else:
+            match+= (n - 1) / 2
+            n=(n - 1) / 2 + 1
+    return match
